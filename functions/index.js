31bb57e8d8df9845123e124751e3ed1e263a9083
EnobongOffiong <edoffiong@gmail.com>
2026-02-24 15:33:37 -0600

    Add protected /me route and verify Firebase auth

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,18 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// sanity check route
 app.get("/health", (req, res) => {
   res.json({ ok: true, message: "Functions API is alive" });
+});
+
+const verifyFirebaseUser = require("./src/middleware/verifyFirebaseUser");
+
+app.get("/me", verifyFirebaseUser, (req, res) => {
+  res.json({
+    uid: req.user.uid,
+    email: req.user.email || null,
+    name: req.user.name || null,
+  });
 });
 
 exports.api = functions.https.onRequest(app);
